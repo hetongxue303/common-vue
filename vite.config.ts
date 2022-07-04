7b14858bdc6f4ba60bf16bf57ef9993218d4a82a
hetongxue303 <1974597686@qq.com>
2022-07-04 14:24:23 +0800

    项目基础配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import * as path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src')
+        }
+    },
+    plugins: [vue()],
+    server: {
+        host: '127.0.0.1',
+        port: 3000,
+        cors: true,
+        open: true,
+        proxy: {
+            '/api': {
+                target: 'http://127.0.0.1:8080',
+                changeOrigin: true,
+                rewrite: (path: string) => path.replace(/^\/api/, '')
+            }
+        }
+    }
 })
